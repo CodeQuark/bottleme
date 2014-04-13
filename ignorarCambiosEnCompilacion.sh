0e6fee3797776b9d4831e2036399893e8a1dcd7d
dengue8830 <dengue8830@gmail.com>
2014-04-13 03:18:16 -0300

    ignorando cambios en compilacion

---
+++ ignorarCambiosEnCompilacion.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+git update-index --assume-unchanged platforms/android/CordovaLib/ant-build/classes.jar
+
+git update-index --assume-unchanged platforms/android/CordovaLib/ant-build/build.prop
+
+git update-index --assume-unchanged platforms/android/ant-build/BottleMe.ap_.d
+
+git update-index --assume-unchanged platforms/android/ant-build/build.prop
+
+git update-index --assume-unchanged platforms/android/ant-build/dexedLibs/classes-43931c3181fb6e0ecab659996e3ed9d0.jar
+
+git update-index --assume-unchanged platforms/android/assets/www/css/appFramework/ios7.css
+
+git update-index --assume-unchanged platforms/android/assets/www/css/appFramework/main.css
+
+git update-index --assume-unchanged platforms/android/assets/www/img/playa.jpg
+
+git update-index --assume-unchanged platforms/android/assets/www/img/playa2.jpg
+
+git update-index --assume-unchanged platforms/android/assets/www/img/playa3.jpg
+
+git update-index --assume-unchanged platforms/android/assets/www/img/playa4.jpg
+
+git update-index --assume-unchanged platforms/android/assets/www/img/playa5.jpg
+
+git update-index --assume-unchanged platforms/android/assets/www/index.html
+
+exit
